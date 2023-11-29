63198d0d634a2eb28a21be7f2d6139d05dd9b1d2
AylenSosa13 <132699509+AylenSosa13@users.noreply.github.com>
2023-11-29 12:15:11 -0300

    Delete inventario galileo directory

--- inventario galileo/borrado.js
+++
@@ -1,9 +0,0 @@
-
-            function confirmarBorrado() {
-            var confirmacion = confirm("¿Estás seguro que quieres borrar este dato?");
-            if (confirmacion) {
-                // Si el usuario presiona "Aceptar", se borra el dato
-                window.location.href = "elim.php?id=<?php echo $row['id']; ?>";
-            }
-            }
-     
